4445881bd062c20ae27c7534f034010c4929feda
l-0-l <fault-tolerant@mail.ru>
2020-04-10 15:42:25 +0300

    Change the player image showing logic, make player a singleton

--- player.py
+++ player.py
@@ -3,9 +3,18 @@ from direction import Direction
 from time import clock
 
 
-class Player(object):
+class Player:
+    """
+    This class is a singleton, as in this game only one player can exist.
+    """
 
-    def __init__(self, x, y, screen, nominal_acceleration, pic_move_right, pic_move_left):
+    __instance = None
+
+    def __init__(self, x, y, screen, nominal_acceleration, max_speed, pic_move_right, pic_move_left):
+        if Player.__instance:
+            raise Exception("Only one player!")
+        else:
+            Player.__instance = self
         self.current_acceleration = 0
         self.current_speed = 0
         self.screen = screen
@@ -15,28 +24,24 @@ class Player(object):
         self.width, self.height = pic_move_right[0].get_rect().size
         self.x = x - self.width/2   # Center of the pic
         self.y = y - self.height/2  # Center of the pic
-        self.current_pic = 0
-        self.frame_time = clock()
+        self.max_speed = max_speed
 
     def set_direction(self, direction):
         """
-        Set acceleration according to the direction
+        Set acceleration according to the direction.
         """
         self.current_acceleration = direction.value * self.nominal_acceleration
 
     def move(self):
         """
-        Move the player for each cycle, making sure we don't go past the walls
+        Move the player for each cycle, making sure we don't go past the walls.
         """
         # Accelerate/decelerate
         if self.current_acceleration != 0:
             # Accelerate
             self.current_speed += self.current_acceleration
-            # Change the image to the next one, assumed that number of images for right and left is identical
-            if abs(self.current_pic + self.get_direction().value) < len(self.pic_move_right):
-                if clock() > self.frame_time:
-                    self.current_pic += self.get_direction().value
-                    self.frame_time = clock() + 0.1  # TODO: magic number here
+            if abs(self.current_speed) > self.max_speed:
+                self.current_speed = copysign(1, self.current_speed) * self.max_speed
         elif self.current_speed != 0:
             # Decelerate
             sign = copysign(1, self.current_speed)
@@ -44,13 +49,6 @@ class Player(object):
             # Set speed to zero if it is very low
             if abs(self.current_speed) < self.nominal_acceleration:
                 self.current_speed = 0
-            # Change the image to the previous one
-            if abs(self.current_pic - self.get_direction().value) >= 0:
-                if clock() > self.frame_time:
-                    self.current_pic -= int(copysign(1, self.current_pic))
-                    self.frame_time = clock() + 0.1  # TODO: magic number here
-        else:
-            self.current_pic = 0
 
         # Calculate the next x axis location
         if 0 < self.x + self.current_speed < self.screen.width - self.width:
@@ -63,29 +61,47 @@ class Player(object):
 
     def get_direction(self):
         """
-        Returns player's direction
+        Returns player's direction.
+        Actually, the direction concept is a bit tricky. This method returns the
+        direction the user wants to move to, even if currently he moves in the opposite
+        direction, but if he doesn't press any button - the actual moving direction
+        is returned.
         """
         if self.current_acceleration > 0:
             direction = Direction.right
         elif self.current_acceleration < 0:
             direction = Direction.left
         elif self.current_speed > 0:
-            direction = Direction.left
-        elif self.current_speed < 0:
             direction = Direction.right
+        elif self.current_speed < 0:
+            direction = Direction.left
         else:
             direction = Direction.none
         return direction
 
     def get_current_pic(self):
         """
-        Returns the current player image
+        Returns the current player image.
         """
-        direction = self.get_direction()
-        if direction == Direction.right:
-            pic = self.pic_move_right[abs(self.current_pic)]
-        elif direction == Direction.left:
-            pic = self.pic_move_left[abs(self.current_pic)]
+        # The current picture is determined by the spaceship speed
+        num_pic = abs(round(self.current_speed / 2))  # TODO: Magic number here
+        total_pics = len(self.pic_move_right) - 1
+        # The number of the picture can't be more than the pictures we have
+        if num_pic >= total_pics:
+            num_pic = total_pics
+
+        # Here the direction is the actual moving direction, rather than the direction
+        # the user wants the spaceship to move as returned by self.get_direction method.
+        direction = copysign(1, self.current_speed)
+
+        # Now we select the actual picture from the lists
+        if direction == Direction.right.value:
+            # Going right
+            pic = self.pic_move_right[num_pic]
+        elif direction == Direction.left.value:
+            # Going left
+            pic = self.pic_move_left[num_pic]
         else:
+            # Not moving
             pic = self.pic_move_right[0]
         return pic
--- space.py
+++ space.py
@@ -8,22 +8,24 @@ WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 600
 BG_COLOR = (0, 128, 128)
 NOMINAL_ACCELERATION = 0.5
+MAX_PLAYER_SPEED = 20
 
-move_right = [pygame.image.load("res/spaceship_N_00.png"),
-              pygame.image.load("res/spaceship_R_01.png"),
-              pygame.image.load("res/spaceship_R_02.png"),
-              pygame.image.load("res/spaceship_R_03.png"),
-              pygame.image.load("res/spaceship_R_04.png"),
-              pygame.image.load("res/spaceship_R_05.png"),
-              pygame.image.load("res/spaceship_R_06.png")]
+pic_move_right = [pygame.image.load("res/spaceship_N_00.png"),
+                  pygame.image.load("res/spaceship_R_01.png"),
+                  pygame.image.load("res/spaceship_R_02.png"),
+                  pygame.image.load("res/spaceship_R_03.png"),
+                  pygame.image.load("res/spaceship_R_04.png"),
+                  pygame.image.load("res/spaceship_R_05.png"),
+                  pygame.image.load("res/spaceship_R_06.png")]
 
-move_left = [pygame.image.load("res/spaceship_N_00.png"),
-             pygame.image.load("res/spaceship_L_01.png"),
-             pygame.image.load("res/spaceship_L_02.png"),
-             pygame.image.load("res/spaceship_L_03.png"),
-             pygame.image.load("res/spaceship_L_04.png"),
-             pygame.image.load("res/spaceship_L_05.png"),
-             pygame.image.load("res/spaceship_L_06.png")]
+pic_move_left = [pygame.image.load("res/spaceship_N_00.png"),
+                 pygame.image.load("res/spaceship_L_01.png"),
+                 pygame.image.load("res/spaceship_L_02.png"),
+                 pygame.image.load("res/spaceship_L_03.png"),
+                 pygame.image.load("res/spaceship_L_04.png"),
+                 pygame.image.load("res/spaceship_L_05.png"),
+                 pygame.image.load("res/spaceship_L_06.png")]
+
 
 class Space(object):
     def __init__(self):
@@ -36,17 +38,21 @@ class Space(object):
                              y=self.screen.height-self.screen.height/10,
                              screen=self.screen,
                              nominal_acceleration=NOMINAL_ACCELERATION,
-                             pic_move_right=move_right,
-                             pic_move_left=move_left)
+                             max_speed=MAX_PLAYER_SPEED,
+                             pic_move_right=pic_move_right,
+                             pic_move_left=pic_move_left)
 
         # Caption and icon
         pygame.display.set_caption("Space")
         pic_logo = pygame.image.load("res/spaceship_N_00.png")
         pygame.display.set_icon(pic_logo)
+
+        # Begin running :)
         self.running = True
 
     def main(self):
         """ Main game loop """
+
         while self.running:
 
             # Check for events
@@ -62,9 +68,11 @@ class Space(object):
                         self.player.set_direction(Direction.right)
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_LEFT:
+                        # Check the direction change is relevant
                         if self.player.get_direction() == Direction.left:
                             self.player.set_direction(Direction.none)
                     elif event.key == pygame.K_RIGHT:
+                        # Check the direction change is relevant
                         if self.player.get_direction() == Direction.right:
                             self.player.set_direction(Direction.none)
 
